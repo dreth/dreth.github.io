7172bab9fb854d11a1cbd36eeb73db6eb0db71e7
Daniel A <dreth@pop-os.localdomain>
2020-11-09 23:15:00 +0100

    removing old cv from site

--- cv/old/script.js
+++
@@ -1,5 +0,0 @@
-function swap_theme(theme, lang) {
-    url = `../cvs/CV-${lang.toUpperCase()}-${theme.toUpperCase()}`
-    document.getElementById('img_doc').src =  `${url}.png`
-    document.getElementById('pdf_doc').href =  `${url}.pdf`
-}
