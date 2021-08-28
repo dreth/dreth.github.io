8fb6abd3f031e065b917dcfbc86d11efb70fa873
dreth <github@dac.ac>
2021-08-28 17:41:38 +0200

    lots of small updates

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// links to all the profiles + email
 var linkedin = 'https://dac.ac/p/linkedin';
 var github = 'https://dac.ac/p/github';
 var blog = 'https://dac.ac/p/blog';
 var home = 'https://dac.ac';
 var status = 'https://dac.ac/status';
-
+var email = 'mailto:contact@dac.ac';
+// detecting the language to abstract language-based links
 let url_lang = window.location.href.split("/");
 url_lang = url_lang[url_lang.length - 1];
 if (url_lang == 'nl') {
@@ -11,6 +13,6 @@ if (url_lang == 'nl') {
 } else {
     var lang = url_lang;
 }
-console.log(url_lang)
-
+// language-based links
 var cv = `${home}/cv/${lang}`;
+var home_lang = `${home}/${lang}`;
