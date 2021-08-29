5d1984bb3561aa568bd1a7a61eb76d35a059d931
dreth <github@dac.ac>
2021-08-29 21:50:35 +0200

    another smallt est

--- blog/blogScript.js
+++ blog/blogScript.js
@@ -3,6 +3,7 @@ var blog = 'https://dac.ac/blog/';
 let art_path = window.location.href.split("/");
 art_path = art_path[art_path.length - 1];
 var art_gh_path = `https://raw.githubusercontent.com/dreth/dreth.github.io/master/blog/${art_path}/article.md`;
+console.log(art_gh_path)
 
 
 // lists of article info
