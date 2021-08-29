3d3a722d14b7ae628f3ced92b943f0ef9e4c5fb2
dreth <github@dac.ac>
2021-08-29 21:44:53 +0200

    some js fixes

--- blog/blogScript.js
+++ blog/blogScript.js
@@ -1,28 +1,29 @@
 // blog links
+var blog = 'https://dac.ac/blog/';
 let art_path = window.location.href.split("/");
 art_path = art_path[art_path.length - 1];
-var art_gh_path = `https://raw.githubusercontent.com/dreth/dreth.github.io/master/blog/${art_path}/article.md`
+var art_gh_path = `https://raw.githubusercontent.com/dreth/dreth.github.io/master/blog/${art_path}/article.md`;
 
 
 // lists of article info
 var articleTitles = [
     'My switch to linux as primary OS in 2020',
     'How a staple of Dominican cuisine, plantains, have helped me manage my Crohn’s Disease'
-]
+];
 var articleTag = [
     'linux2020',
     'plantains'
-]
+];
 // links for the article MD files and article URL
-var artList = '<ul>'
+var artList = '<ul>';
 for (let i = 0; i <= articleTag.length-1; i++) {
     // generate article URL
     let articleURL = `https://dac.ac/blog/${articleTag[i]}/`;
 
     // append to article list html object
-    artList += `<li><a class="c" href="${articleURL}">${articleTitles[i]}</a></li><br>`
+    artList += `<li><a class="c" href="${articleURL}">${articleTitles[i]}</a></li><br>`;
 }
-artList += '</ul>'
-console.log(artList)
+artList += '</ul>';
+
 // appending the list of articles
 document.getElementById("articleList").innerHTML = artList;
