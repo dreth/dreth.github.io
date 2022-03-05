c82b97990087bca01fcbae5644ff83d33bc87821
dreth <github@dac.ac>
2022-03-05 19:23:48 +0100

    add other links and re-organize link logic

--- cool_links/scripts/coolLinksScript.js
+++ cool_links/scripts/coolLinksScript.js
@@ -13,3 +13,72 @@ switch(browserLocale) {
 
 // setting h1 title
 document.getElementById("linksTitle").innerHTML = `<h3>⛓️ ${linksTitle}</h3>`
+
+// base link
+const wikipediaBaseLink = 'wikipedia.org/wiki/';
+const wiktionaryBaseLink = 'wiktionary.org/wiki/';
+
+// links
+$.getJSON('/cool_links/links/links.json', (links) => {
+
+    // ------------------------------------------ WIKIPEDIA
+    // generate wikipedia link list
+    var wikipediaArticles = links['wikipedia']; 
+    var artList = '<ul>';
+    for (const name of Object.keys(wikipediaArticles).reverse()) {
+        // extract language of article
+        let articleLang = wikipediaArticles[name].slice(0,2);
+
+        // create article URL
+        var articleURL = `https://${articleLang}.${wikipediaBaseLink}${wikipediaArticles[name].slice(3)}` 
+
+        // append to article list html object
+        artList += `<span>🔗</span> <a class="b" href="${articleURL}">${name}</a><br><br>`;
+    }
+    artList += '</ul><hr>';
+
+    // appending the list of articles
+    if (document.getElementById("wikipediaLinkList")) {
+        document.getElementById("wikipediaLinkList").innerHTML = artList;
+    }
+
+    // ------------------------------------------ WIKTIONARY
+    // generate wiktionary link list
+    var wiktionaryArticles = links['wiktionary'];
+    var artList = '<ul>';
+    for (const name of Object.keys(wiktionaryArticles).reverse()) {
+        // extract language of article
+        let articleLang = wiktionaryArticles[name].slice(0,2);
+
+        // create article URL
+        var articleURL = `https://${articleLang}.${wiktionaryBaseLink}${wiktionaryArticles[name].slice(3)}` 
+
+        // append to article list html object
+        artList += `<span>🔗</span> <a class="b" href="${articleURL}">${name}</a><br><br>`;
+    }
+    artList += '</ul><hr>';
+
+    // appending the list of articles
+    if (document.getElementById("wiktionaryLinkList")) {
+        document.getElementById("wiktionaryLinkList").innerHTML = artList;
+    }
+
+    // ------------------------------------------ ANYTHING ELSE
+    // generate wiktionary link list
+    var wiktionaryArticles = links['other_sites'];
+    var artList = '<ul>';
+    for (const name of Object.keys(wiktionaryArticles).reverse()) {
+        // create article URL
+        var articleURL = wiktionaryArticles[name] 
+
+        // append to article list html object
+        artList += `<span>🔗</span> <a class="b" href="${articleURL}">${name}</a><br><br>`;
+    }
+    artList += '</ul><hr>';
+
+    // appending the list of articles
+    if (document.getElementById("otherSitesLinkList")) {
+        document.getElementById("otherSitesLinkList").innerHTML = artList;
+    }
+
+})
---
+++ cool_links/scripts/links.py
@@ -0,0 +1,92 @@
+from urllib import parse
+import json
+from bs4 import BeautifulSoup
+import requests
+
+
+# 1 = wikipedia
+# 2 = anything
+# 3 = wiktionary
+
+def get_links(type):
+    input('did you check everything?')
+
+    with open('./cool_links/links/links.json', 'r') as f:
+        links = json.load(f)
+
+    if type == 1:
+        with open('./cool_links/scripts/links_to_process.txt') as f:
+
+            for i,line in enumerate(f.read().splitlines()):
+                
+                # get language
+                try:
+                    lang = line.split('https://')[1][0:2].upper()
+                except:
+                    continue
+
+                # get page name
+                try:
+                    page_name = parse.unquote(line).split('/wiki/')[1].replace('_',' ')
+                except:
+                    continue
+                
+                # get url elements after /wiki/
+                try:
+                    url_dir = line.split('/wiki/')[1]
+                except:
+                    continue
+
+                # assemble and print elements
+                links['wikipedia'][page_name] = f"{lang}_{url_dir}"
+
+        with open('./cool_links/links/links.json', 'w') as f:
+            json.dump(links, f, indent=4, ensure_ascii=False)
+    
+    elif type == 2:
+        with open('./cool_links/scripts/links_to_process.txt') as f:
+
+            for i,line in enumerate(f.read().splitlines()):
+
+                # get site
+                soup = BeautifulSoup(requests.get(line).text, 'html.parser')
+
+                # get URL
+                for title in soup.find_all('title'):
+                    links['other_sites'][title.get_text()] = line 
+
+    elif type == 3:
+        with open('./cool_links/scripts/links_to_process.txt') as f:
+            
+            for i,line in enumerate(f.read().splitlines()):
+                
+                # get language
+                try:
+                    lang = line.split('https://')[1][0:2].upper()
+                except:
+                    continue
+
+                # get page name
+                try:
+                    page_name = parse.unquote(line).split('/wiki/')[1].replace('_',' ')
+                except:
+                    continue
+                
+                # get url elements after /wiki/
+                try:
+                    url_dir = line.split('/wiki/')[1]
+                except:
+                    continue
+
+                # assemble and print elements
+                links['wiktionary'][page_name] = f"{lang}_{url_dir}"
+    
+    else:
+        exit()
+
+    with open('./cool_links/links/links.json', 'w') as f:
+            json.dump(links, f, indent=4, ensure_ascii=False)
+
+
+# run function
+get_links(2)
