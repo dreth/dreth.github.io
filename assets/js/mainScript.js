266a4bc8715fcfc8754ae367735f428b7df4d0a2
dreth <github@dac.ac>
2022-07-13 01:22:55 +0200

    most of the stuff gotta fix: - in-out for background in subscribe box - add pages for when ppl interact with mc - just check mc to see if theres anything cooler - test an email campaign

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -449,6 +449,41 @@ function removeBlogIcon() {
     })
 }
 
+// MAILCHIMP STUFF --------------------------------------
+$("#mailchimpStuff").html(`
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="https://dac.us12.list-manage.com/subscribe/post?u=2dc3eb13fc6803ee768b95c77&amp;id=97eb89d742" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<div id="mc_embed_signup_scroll">
+<div class="mc-field-group">
+	<p for="mce-EMAIL">
+        <input type="email" value="" name="EMAIL" class="emailField" id="mce-EMAIL" placeholder="e-mail">
+    </p>
+</div>
+	<div id="mce-responses" class="clear foot">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2dc3eb13fc6803ee768b95c77_97eb89d742" tabindex="-1" value=""></div>
+        <div class="optionalParent">
+            <div class="clear foot" onmousenter="mcSubscribeIn()" onmouseleave="mcSubscribeOut()">
+                <h3 class="inline"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"  class="subscribeButton"></h3>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+`)
+
+// background color change for subscribe button
+function mcSubscribeIn() {
+    $("#mcStuff").addClass("alt2")
+}
+function mcSubscribeOut() {
+    $("#mcStuff").removeClass("alt2")
+}
 
 // ADD HOMEPAGE EVENTS ----------------------------------
 // change text of about me, cool links and playlists on hover or clicking the div
