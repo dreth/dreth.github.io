462887354b08ee25de5cfa192224dd343e2c295e
dreth <github@dac.ac>
2022-12-31 19:25:13 +0100

    correct lang preview on about me page

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -381,8 +381,6 @@ function updateLang(l) {
     // if on projects page
     if (document.getElementById("projectsTitleBar")) {
         updateProjectsList()
-    } else if (document.getElementById("aboutMeTitleBar")) {
-        loadCV(l)
     }
 }
 
